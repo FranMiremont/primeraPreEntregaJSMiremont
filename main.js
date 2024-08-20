6a956382003c2f8f68e7c32818943ccb7521e20f
Fran Miremont <franmiremont@gmail.com>
2024-08-20 20:59:50 -0300

    primer commit

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
+// Preguntar nombre del usuario
+var nombre = prompt("¿Cuál es su nombre?");
+
+// Saludar al usuario
+alert("Bienvenido, " + nombre + " ,espero estés teniendo un bien día");
+
 // Función para mostrar el menú de opciones
 function mostrarMenu() {
   alert("Menú de opciones:\n1. Hamburguesa ($10)\n2. Pizza ($15)\n3. Ensalada ($8)\n4. Terminar");
 }
 
 // Función para procesar la elección del usuario
-function procesarElección() {
-  var elección = prompt("Ingrese su elección (1-4):");
-  if (elección == 1) {
+function procesarEleccion() {
+  var eleccion = prompt("Ingrese su elección (1-4):");
+  if (eleccion == 1) {
     alert("Usted ha elegido la hamburguesa. Precio: $10");
-  } else if (elección == 2) {
+  } else if (eleccion == 2) {
     alert("Usted ha elegido la pizza. Precio: $15");
-  } else if (elección == 3) {
+  } else if (eleccion == 3) {
     alert("Usted ha elegido la ensalada. Precio: $8");
-  } else if (elección == 4) {
-    alert("Gracias por su pedido. ¡Hasta luego!");
+  } else if (eleccion == 4) {
+    alert("Gracias por su pedido. Buen provecho");
     return false;
   } else {
     alert("Opción inválida. Por favor, inténtelo de nuevo.");
@@ -24,7 +30,7 @@ function procesarElección() {
 // Ciclo para mostrar el menú y procesar la elección del usuario
 while (true) {
   mostrarMenu();
-  if (!procesarElección()) {
+  if (!procesarEleccion()) {
     break;
   }
 }
